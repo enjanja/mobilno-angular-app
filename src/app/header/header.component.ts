34990cf2fb2dc0284bc76dc66a84791cbd5c51e6
Sanja Nikolic <sanjanikolic9634@gmail.com>
2021-03-15 16:10:44 +0100

    rucno kreiranje komponente

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    //moze i samo template
+    //izgled stranice def u h.c.html fajlu
+    styleUrls: ['./header.component.css']
+    //moze i samo style
+    //def stila nase app
+
+})
+
+export class HeaderComponent{
+
+}
